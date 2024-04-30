13f1a8acc01c4a942921cb45dcdfc60a8c539137
NikolaySigov <nikolay.sigov@gmail.com>
2024-04-30 17:00:42 +0300

    add employee functionality

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    val numberOfOrders = 75;
+    val thankMessage = "Thank you for your order!"
+    println("$numberOfOrders orders have been received. $thankMessage");
+
+    var numberOfEmployee = 2000;
+    //println("$numberOfEmployee employees are currently working here.")
+
+    fun dismissEmployee(): Unit {
+        numberOfEmployee--;
+    }
+
+    dismissEmployee()
+    println("$numberOfEmployee employees are currently working here.")
 }
+
