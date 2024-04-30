b81e9ba09129c66d1455717633162e562ce2262c
NikolaySigov <nikolay.sigov@gmail.com>
2024-04-30 17:20:43 +0300

    calculate and print flight time

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,22 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println(
+        """
+      The spacecraft «Vostok» was launched
+      $year year
+      $hour hours
+      $minute minutes
+    """.trimIndent()
+    )
+
+    val flightTimeInMinutes = 108
+    hour += flightTimeInMinutes / 60
+    minute += flightTimeInMinutes % 60
+
+    println("The spacecraft landed at $hour:$minute")
+
 }
