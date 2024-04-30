717cf8a0a77cd25c9fdf91da7c7c34fce6389a47
NikolaySigov <nikolay.sigov@gmail.com>
2024-04-30 22:06:56 +0300

    add flight calculations

---
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println(
+        """
+      The spacecraft «Vostok» was launched
+      $year year
+      $hour hours
+      $minute minutes
+    """.trimIndent()
+    )
+
+    val flightTimeInMinutes = 108
+    hour += flightTimeInMinutes / 60
+    minute += flightTimeInMinutes % 60
+
+    println("The spacecraft landed at $hour:$minute")
+}
