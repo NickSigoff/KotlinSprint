435313fca3f8edc5b2b11f5f481fe61d06ce38dd
NikolaySigov <nikolay.sigov@gmail.com>
2024-05-03 10:47:11 +0300

    delete if condition

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -8,11 +8,9 @@ fun main() {
     var arrivalHour = (departureHour + travelTimeInMinutes / 60)
     var arrivalMinute = departureMinute + travelTimeInMinutes % 60
 
-    if (arrivalMinute >= 60) {
-        arrivalHour += 1
-        arrivalMinute -= 60
-    }
+    arrivalHour += arrivalMinute / 60
     arrivalHour %= 24
+    arrivalMinute %= 60
 
     print("Arrival time: ${String.format("%02d", arrivalHour)}:${String.format("%02d", arrivalMinute)}")
 }
