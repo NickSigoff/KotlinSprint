f64cde24f2e70cafd7ae3a9344b250528c49934d
NikolaySigov <nikolay.sigov@gmail.com>
2024-05-03 23:10:47 +0300

    update calculation equation

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -2,12 +2,12 @@ package org.example.lesson_2
 
 fun main() {
     val initialSumOfMoney = 70_000
-    val percent = 0.167
+    val percent = 16.7
     val bankDepositDurationInYears = 20
 
     var resultSum = initialSumOfMoney.toDouble()
     for (i in 1..bankDepositDurationInYears) {
-        resultSum += resultSum * percent
+        resultSum += resultSum * percent / 100
     }
 
     println("Result sum of money: ${String.format("%.3f", resultSum)}")
