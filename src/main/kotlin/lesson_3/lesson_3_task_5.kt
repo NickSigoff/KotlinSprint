fac9993923aec368bc59f296e59e18c600cde8ff
NikolaySigov <nikolay.sigov@gmail.com>
2024-05-06 22:48:42 +0300

    add second parameter to split function

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -5,11 +5,10 @@ fun main() {
     val pointsDelimiter = "-"
 
     val moveData = "D2-D4;0"
-    val splitMoveData = moveData.split(dataDelimiter)
-    val moveNumber = splitMoveData[1]
-    val movePoints = splitMoveData[0].split(pointsDelimiter)
-    val pointFrom = movePoints[0]
-    val pointTo = movePoints[1]
+    val splitMoveData = moveData.split(pointsDelimiter,dataDelimiter)
+    val pointFrom = splitMoveData[0]
+    val pointTo = splitMoveData[1]
+    val moveNumber = splitMoveData[2]
 
     println("Move: $moveNumber")
     println("From: $pointFrom")
