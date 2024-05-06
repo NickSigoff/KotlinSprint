2e5e56437367b4bba560a8628f21f2815d1e1e87
NikolaySigov <nikolay.sigov@gmail.com>
2024-05-06 22:40:13 +0300

    convert val -> const

--- src/main/kotlin/lesson_4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_4
 
+const val NUMBER_OF_TABLES = 13
 fun main() {
-    val tablesNumber = 13
     val tablesBookedToday = 13
     val tablesBookedTomorrow = 9
-    println("Доступность столиков на сегодня: ${tablesNumber - tablesBookedToday > 0}\nДоступность столиков на завтра: ${tablesNumber - tablesBookedTomorrow > 0}")
+    println("Доступность столиков на сегодня: ${NUMBER_OF_TABLES - tablesBookedToday > 0}\nДоступность столиков на завтра: ${NUMBER_OF_TABLES - tablesBookedTomorrow > 0}")
 
 
 }
