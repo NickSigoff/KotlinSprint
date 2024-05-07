55cd61f58e2175d804a68dec16328dc7528b422c
NikolaySigov <nikolay.sigov@gmail.com>
2024-05-07 11:34:15 +0300

    create const conditions

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,12 +1,21 @@
 package org.example.lesson_4
 
+const val FAVOURABLE_HUMIDITY_PERCENT = 20
+const val NOT_FAVOURABLE_SEASON = "Зима"
+const val NECESSARILY_SUNNY_WEATHER = true
+const val NECESSARILY_OPENED_TENT = true
+
 fun main() {
     val isSunnyWeather = true
     val isTentOpened = true
     val humidityPercent = 20
-    val season = "Зима"
+    val season = "Зма"
 
-    val areFavourableConditions = isSunnyWeather && isTentOpened && humidityPercent == 20 && season != "Зима"
+    val areFavourableConditions =
+        isSunnyWeather == NECESSARILY_SUNNY_WEATHER &&
+                isTentOpened == NECESSARILY_OPENED_TENT &&
+                humidityPercent == FAVOURABLE_HUMIDITY_PERCENT &&
+                season != NOT_FAVOURABLE_SEASON
 
     println("Благоприятные ли условия сейчас для роста бобовых? $areFavourableConditions")
 }
